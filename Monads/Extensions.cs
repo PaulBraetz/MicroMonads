9dad68a236459c10677214e881d147a119317f7e
PauBra <PauBra@PSVM127>
2022-07-21 15:22:44 +0200

    Add project files.

---
+++ Monads/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monads
+{
+    internal static class Extensions
+    {
+        public static IEnumerable<T> GetEmptyIfDefault<T>(this IEnumerable<T> collection)
+        {
+            return collection ?? Array.Empty<T>();
+        }
+        public static Boolean IsEmptyOrDefault<T>(this IEnumerable<T> collection)
+        {
+            return !(collection?.Any() ?? false);
+        }
+    }
+}
