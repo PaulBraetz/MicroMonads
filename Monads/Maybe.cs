d7459f544084b64f9c7f0fce2b62a3f215eb638a
Brätz, Paul <paul.braetz@th-wildau.de>
2022-07-22 17:41:07 +0200

    added readme,changed project name

--- Monads/Maybe.cs
+++ Monads/Maybe.cs
@@ -1,37 +1,37 @@
 ﻿namespace Monads
 {
-    public readonly struct Maybe<T> : IEquatable<Maybe<T>>
-    {
-        public Maybe(T value)
-        {
-            Value = value;
-            HasValue = true;
-        }
+	public readonly struct Maybe<T> : IEquatable<Maybe<T>>
+	{
+		public Maybe(T value)
+		{
+			Value = value;
+			HasValue = true;
+		}
 
-        public static readonly Maybe<T> Empty = default;
+		public static readonly Maybe<T> Empty = default;
 
-        public readonly T Value;
+		public readonly T Value;
 
-        public readonly Boolean HasValue;
+		public readonly Boolean HasValue;
 
-        public static Maybe<T> Unit(Func<T> valueFactory, Boolean hasValue) => hasValue ? Just(valueFactory.Invoke()) : Nothing();
-        public static Maybe<T> Just(T value) => new(value);
-        public static Maybe<T> Nothing() => Empty;
+		public static Maybe<T> Unit(Func<T> valueFactory, Boolean hasValue) => hasValue ? Just(valueFactory.Invoke()) : Nothing();
+		public static Maybe<T> Just(T value) => new(value);
+		public static Maybe<T> Nothing() => Empty;
 
-        public Maybe<TResult> Bind<TResult>(Func<T, Maybe<TResult>> function) => HasValue ? function.Invoke(Value) : Maybe<TResult>.Nothing();
-        public Maybe<TResult> Bind<TResult>(Func<T, TResult> function) => Bind(v => Maybe<TResult>.Just(function.Invoke(v)));
+		public Maybe<TResult> Bind<TResult>(Func<T, Maybe<TResult>> function) => HasValue ? function.Invoke(Value) : Maybe<TResult>.Nothing();
+		public Maybe<TResult> Bind<TResult>(Func<T, TResult> function) => Bind(v => Maybe<TResult>.Just(function.Invoke(v)));
 
-        public override Boolean Equals(Object? obj) => obj is Maybe<T> maybe && Equals(maybe);
-        public Boolean Equals(Maybe<T> other) => HasValue == other.HasValue && EqualityComparer<T>.Default.Equals(Value, other.Value);
-        public override Int32 GetHashCode() => HashCode.Combine(Value, HasValue);
+		public override Boolean Equals(Object? obj) => obj is Maybe<T> maybe && Equals(maybe);
+		public Boolean Equals(Maybe<T> other) => HasValue == other.HasValue && EqualityComparer<T>.Default.Equals(Value, other.Value);
+		public override Int32 GetHashCode() => HashCode.Combine(Value, HasValue);
 
-        public static Boolean operator ==(Maybe<T> left, Maybe<T> right) => left.Equals(right);
-        public static Boolean operator !=(Maybe<T> left, Maybe<T> right) => !(left == right);
+		public static Boolean operator ==(Maybe<T> left, Maybe<T> right) => left.Equals(right);
+		public static Boolean operator !=(Maybe<T> left, Maybe<T> right) => !(left == right);
 
-        public override String ToString()
-        {
-            var value = HasValue ? $"\"Just {Value}\"" : "\"Nothing\"";
-            return $"{{Value={value}}}";
-        }
-    }
+		public override String ToString()
+		{
+			var value = HasValue ? $"\"Just {Value}\"" : "\"Nothing\"";
+			return $"{{Value={value}}}";
+		}
+	}
 }
---
+++ README.md
@@ -0,0 +1,66 @@
+# MicroMonads
+
+MicroMonads provides simple Monads for use in .Net projects.
+
+## Featured Monads
+
+1. Maybe
+2. Carry (also nown as log)
+
+## Installation
+
+Nuget Gallery: https://www.nuget.org/packages/RhoMicro.MicroMonads
+
+.Net CLI: `dotnet add package RhoMicro.MicroMonads --version M.m.p`
+
+Package Manager: `Install-Package RhoMicro.MicroMonads -Version M.m.p`
+
+## How To Use `Maybe<T>`
+
+### Unit Functions
+
+Instantiate a new `Maybe<T>` using the unit functions `Maybe<T>.Unit`, `Maybe<T>.Just` or `Maybe<T>.Nothing`
+
+```cs
+Maybe<Int32> a = Maybe<Int32>.Unit(valueFactory: () => 5, hasValue: true);
+Maybe<Int32> b = Maybe<Int32>.Unit(valueFactory:() => 3, hasValue: false);
+```
+*which is equivalent to*
+```cs
+Maybe<Int32> a = Maybe<Int32>.Just(value: 5);
+Maybe<Int32> b = Maybe<Int32>.Nothing();
+```
+
+Note that `Maybe<T>.Nothing` will not initialize `Maybe<T>.Value`.
+
+### Usage
+
+Construct a function that returns a `Maybe<T>`:
+
+```cs
+Func<Int32, Int32, Maybe<Int32>> safeDivision = (x, y) => Maybe<Int32>.Unit(() => x/y, y != 0);
+
+Maybe<Int32> result = safeDivision.Invoke(12, 3);
+Console.WriteLine(result); //Output: "{Value="Just 4"}"
+
+result = safeDivision(12, 0);
+Console.WriteLine(result); //Output: "{Value="Nothing"}"
+```
+
+### Bind Function
+
+Use the `Maybe<T>.Bind` function to conditionally operate on a wrapped value:
+
+```cs
+Function<Int32, Maybe<Int32>> addTen = v => Maybe<Int32>.Just(v + 10);
+
+Maybe<Int32> just12 = Maybe<Int32>.Just(12);
+
+Maybe<Int32> sum = just12.Bind(addTen);
+Console.WriteLine(sum); //Output: "{Value="Just 22"}"
+
+Maybe<Int32> nothing = Maybe<Int32>.Nothing();
+
+sum = nothing.Bind(addTen);
+Console.WriteLine(sum); //Output: "{Value="Nothing"}"
+```
